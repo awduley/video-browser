dbb6e33e4d86a308b16d3d5c74b5bed14607f133
Andrew Duley <duleyalaska@gmail.com>
2021-07-15 08:28:55 -0400

    backup to transfer computers

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <SearchBar @termChange="onTermChange"></SearchBar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import axios from 'axios';
+  import SearchBar from './components/SearchBar';
+  const API_KEY = 'AIzaSyBvk85XnK68XnLoM6h0RiAS8L7y6vI_eSs';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: 'App',
+    components: {
+      SearchBar
+    },
+    methods: {
+      onTermChange(searchTerm) {
+        axios.get('https://www.googleapis.com/youtube/v3/search', {
+          params: {
+            key: API_KEY,
+            type: 'video',
+            part: 'snippet',
+            q: searchTerm
+          }
+        }).then(res => console.log(res))
+      }
+    }
+  };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
